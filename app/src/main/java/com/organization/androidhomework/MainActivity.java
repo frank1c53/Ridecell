e59cd1f13a9c8bc3bceee81314e047ad367d7ad7
Frank Fernandes <frank.fernandes@atos.net>
2021-05-29 14:14:35 +0530

    Login issue resolved server side thing is working we are getting response

--- app/src/main/java/com/organization/androidhomework/MainActivity.java
+++ app/src/main/java/com/organization/androidhomework/MainActivity.java
@@ -2,6 +2,9 @@ package com.organization.androidhomework;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,9 +14,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
 public class MainActivity extends AppCompatActivity {
     Button signUp,login;
     EditText passText,emailText;
+    JSONObject obj = new JSONObject();
+    private ProgressDialog progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +47,11 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                emailValidator(emailText);
+                try {
+                    emailValidator(emailText,passText);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
@@ -37,10 +60,11 @@ public class MainActivity extends AppCompatActivity {
 
     // the function which triggered when the VALIDATE button is clicked
     // which validates the email address entered by the user
-    public void emailValidator(EditText etMail) {
+    public void emailValidator(EditText etMail,EditText pass) throws JSONException {
 
         // extract the entered data from the EditText
         String emailToText = etMail.getText().toString();
+        String passText = pass.getText().toString();
 
         // Android offers the inbuilt patterns which the entered
         // data from the EditText field needs to be compared with
@@ -48,12 +72,77 @@ public class MainActivity extends AppCompatActivity {
         // the EMAIL_ADDRESS, which is implemented same below
         if (!emailToText.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(emailToText).matches()) {
             Toast.makeText(this, "Email Verified !", Toast.LENGTH_SHORT).show();
+
+            obj.put("email",emailToText);
+            obj.put("password",passText);
+            deciderState();
+
         } else {
             Toast.makeText(this, "Enter valid Email address !", Toast.LENGTH_SHORT).show();
         }
     }
-}
 
+
+
+
+
+    public void deciderState(){
+
+        Toast.makeText(this, "Decider State", Toast.LENGTH_SHORT).show();
+        System.out.println(obj);
+        //networkHandler.createRequest((NetworkHandler.NetworkManagerListener) this, requestParameters,Tags.Employee_Details);
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.POST,
+                Tags.User_login,obj,
+                new com.android.volley.Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        progressBar.dismiss();
+                        Log.d("Response", response.toString());
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setCancelable(true);
+                        builder.setTitle("User Entries");
+                        builder.setMessage(response.toString());
+                        builder.show();
+                        JSONArray value ;
+                        JSONObject nameObject = null;
+                        JSONObject Data =new JSONObject();
+
+
+
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Response Error", error.toString());
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setCancelable(true);
+                        builder.setTitle("User Entries");
+                        builder.setMessage(error.toString());
+                        builder.show();
+                        JSONArray value ;
+                        JSONObject nameObject = null;
+                        JSONObject Data =new JSONObject();
+                        progressBar.dismiss();
+                    }
+                }
+        );
+        objectRequest.setRetryPolicy(new DefaultRetryPolicy(
+                0,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        requestQueue.add(objectRequest);
+
+
+        progressBar = ProgressDialog.show(MainActivity.this, "Please wait..", "Loading...");
+
+
+
+    }
+}
 
 
 
---
+++ app/src/main/java/com/organization/androidhomework/Tags.java
@@ -0,0 +1,88 @@
+package com.organization.androidhomework;
+
+public class Tags {
+
+	public static final String CONTENT_TYPE = "Content-Type";
+	public static final String ACCEPT = "Accept";
+	public static final String Null_responce = "Null responce";
+
+	public static final String DATA = "DATA";
+	public static final String STATUS = "STATUS";
+	public static final String STATUS_CODE = "STATUS_CODE";
+	public static final String STATUS_MESSAGE = "STATUS_MESSAGE";
+
+	public static final int STATUS_CODE_0 = 0;
+	public static final int STATUS_CODE_OK = 200;
+	public static final String STATUS_MESSAGE_0 = "MDMResponse is Null";
+	public static final int STATUS_CODE_TOKEN_EXPIRED = 401;
+	public static final int STATUS_CODE_BAD_REQUEST = 400;
+	public static final int STATUS_CODE_RESOURCE_UNAVAILABLE = 404;
+	public static final int STATUS_CODE_INTERNAL_SERVER_ERROR = 500;
+	public static final int STATUS_CODE_ACTION_FAILED = 300;
+	public static final int STATUS_CODE_NO_CONTENT = 204;
+	public static final int STATUS_CODE_SERVICE_UNAVAILABLE = 503;
+	public static final int STATUS_CODE_IDENTIFIER_CONFLICT = 409;
+	public static final int STATUS_CODE_ACCOUNT_LOCKED = 600;
+	public static final int STATUS_CODE_NOT_BYOD_USER = 601;
+	public static final int STATUS_CODE_BLACKLISTED_OR_RETIRED = 602;
+
+	public static final int STATUS_CODE_TIMEOUT = 803;
+	public static final int STATUS_CODE_CONNECTION = 804;
+	public static final int STATUS_CODE_SOCKET_TIMEOUT = 805;
+	public static final int STATUS_CODE_IOEXCEPTION = 801;
+	
+	//public static final String Upload_Service_URI="http://10.128.11.81:8080/MyCab/rest/Nfc/insertData";
+	
+	//public static final String Upload_Service_URI1="http://10.128.11.81:8080/MyCab/rest/Nfc/GetEmp";
+	
+	//public static final String Upload_Service_URI3="http://10.128.11.81:8080/MyCab/rest/Roster/RosterTime";
+	
+	//public static final String Upload_Service_URI4="http://10.128.11.81:8080/MyCab/rest/Nfc/GetEmpRoster";
+	
+	//public static final String Car_Details_Service = "http://10.128.11.81:8080/MyCab/rest/Nfc/GetCarDetails";
+	
+	//public static final String Get_Employee_Details_On_Swipe = "http://103.16.6.236:8043/MyCab/rest/Nfc/GetEmp";
+	
+	//public static final String Employee_Details ="https://ismobi.syntel.in/DiwaliGift/rest/diwaligift";
+	
+	//public static final String Employee_Details = "http://10.128.11.185:8080/MyCabNfctms/rest/nfctms/insertTMSdata";
+	
+	//public static final String Employee_Details = "http://10.128.132.252:8080/NfcTms/rest/nfctms";
+	
+    //public static final String Employee_Details = "http://10.128.11.81:8080/MyCabNfctms/rest/nfctms/";
+	
+	public static final String User_login= "http://blooming-stream-45371.herokuapp.com/api/v2/people/authenticate";
+
+	public static final String Insert_Details = "https://dms-uat.myatos-syntel.net/EmergencySOS/rest/login1/submitalert";
+
+	public static final String Update_Details = "https://dms-uat.myatos-syntel.net/EmergencySOS/rest/login1/stopalert";
+
+	public static final String Token_Details = "https://dms-uat.myatos-syntel.net/EmergencySOS/rest/pwadata/submittoken";
+
+	public static final String Alert_Details = "https://dms-uat.myatos-syntel.net/EmergencySOS/rest/pwadata/submittoken";
+	
+	/*public static final String Nfc_Captured_Data_Insert = "http://103.16.6.236:8043/MyCab/rest/Nfc/insertData";
+	
+	public static final String Nfc_Captured_Data_Insert_Mumbai = "http://103.16.6.236:8043/MyCab/rest/Nfc/insertDataMumbai";
+	
+	public static final String Roster_Time = "http://103.16.6.236:8043/MyCab/rest/Roster/RosterTime";
+	
+	public static final String Employee_Roster_Detail = "http://103.16.6.236:8043/MyCab/rest/Nfc/GetEmpRoster";
+	
+	public static final String Car_Details_Service = "http://103.16.6.236:8043/MyCab/rest/Nfc/GetCarDetails";
+	
+	public static final String Active_Trip_Id = "http://103.16.6.236:8043/MyCab/rest/Nfc/gettripid";
+	
+	public static final String Active_Flap_No = "http://103.16.6.236:8043/MyCab/rest/Nfc/getflapno";*/
+	
+	/*public static final String Upload_Service_URI="https://ismobi.syntel.in/MyCab/rest/Nfc/insertData";
+	public static final String Upload_Service_URI1="https://ismobi.syntel.in/MyCab/rest/Nfc/GetEmp";*/
+	
+	
+	/*public static final String Login_Service_URI="idea_junction.person";
+	public static final String Submit_Service_URI="idea_junction.insert";
+	public static final String Feedback_Service_URI="idea_junction.feed";
+	public static final String DropDown_Data_Service_URI="idea_junction.dbc";
+	public static final String View_Idea_Service_URI="idea_junction.viewidea";*/
+	
+}
